8d234af8379259a7f08e0035b77c577fede916de
Andrew Bloom <andrew@andrewkbloom.com>
2014-01-28 23:24:36 -0600

    getting the app to boot

--- watcher.rb
+++ watcher.rb
@@ -1,3 +1,8 @@
 #!/usr/bin/env ruby
 
-puts "great success"
+video_dir = File.expand_path File.join File.dirname(__FILE__), ".."
+
+while true
+  system "omxplayer #{video_dir}/H264_test7_voiceclip_mp4_480x360.mp4"
+  sleep 5
+end
